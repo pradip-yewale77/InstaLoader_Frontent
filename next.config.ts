cc33d3ce7bb61acc8298f35e60ecedcaabf3bb3d
pradip-yewale77 <yewalepradip77@gmail.com>
2025-07-09 13:37:10 +0530

    Configured next.config.ts for Netlify static export

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export", // ✅ This enables static HTML export for Netlify
 };
 
 export default nextConfig;
